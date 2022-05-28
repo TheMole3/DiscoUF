3186ebd2694d5894a44b16d53e78199273c1a922
Hugo Arnlund <hugomelo04@gmail.com>
2022-05-29 00:08:10 +0200

    Spelling fixes

--- client/src/tabs/discokort.js
+++ client/src/tabs/discokort.js
@@ -3,7 +3,7 @@ import styles from './tabStyle'
 function discokort() {
   return (
     <div style={styles.text}>
-        För köp i kiosken används Discokort, discots egna stämpelkort som går att ladda i förväg via förbetalning eller i entréen med swish eller kontanter.
+        För köp i kiosken används Discokort, discots egna stämpelkort som går att ladda i förväg via förbetalning eller i entréen med swish och kontanter.
         <br/><br/>I dagens kontantlösa samhälle vill vi gärna att så många som möjligt förbetalar eller använder Swish.
 
         <br/><br/>Med discokortet behöver inte barnen bära runt på kontanter inne på discot utan de har istället sitt discokort som de i kiosken stämplar av när pengar används.
