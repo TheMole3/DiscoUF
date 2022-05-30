6b7e0acfe41f82e46289914d6d42939e88aab181
Hugo Arnlund <hugomelo04@gmail.com>
2022-05-30 23:54:34 +0200

    Added authentication
    Actually a descriptive message this time

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3010
+const express = require('express');
+const app = express();
+const config = require('./config.json');
+const port = config.port;
+
+const auth = require('./auth/auth'); // Import auth code
 
 /*
 Public endpoints
@@ -28,6 +31,27 @@ Delete booking by id
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/login', (req, res) => { // Microsoft Azure login endpoint
+    auth.auth.getConsentLink((link) => { // Get a microsoft auth consent link
+        res.redirect(link); // Redirect user to authenticate at microsoft
+        //res.send("Login öppnas på måndag");
+    });
+});
+
+app.get('/redirect', (req, res) => { // Microsoft Azure Auth redirect endpoint
+    auth.auth.redirect({ // Handle user
+        req: req,
+        res: res,
+    }, (success) => {
+        if(success) { // If succesfully logged in
+            res.redirect('/'); // Redirect to portal
+        }
+        else {
+            res.send('Login error! Please contact the administrator');
+        }
+    });
+});
 
 app.listen(port, () => {
   console.log(`DISCO UF Server listening on port ${port}`)
