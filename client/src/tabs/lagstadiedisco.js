80ac39a01c3179d28f9cb016159bc5300a0f47d0
Hugo Arnlund <hugomelo04@gmail.com>
2022-08-29 08:30:45 +0200

    Commit

--- client/src/tabs/lagstadiedisco.js
+++ client/src/tabs/lagstadiedisco.js
@@ -4,8 +4,8 @@ function lagstadiedisco() {
   return (
     <div style={styles.text}>
         Discot för lågstadieelever börjar <span style={styles.bold}>17:00</span> och slutar <span style={styles.bold}>18:45</span>
-        <br/>Inträdet är <span style={styles.bold}>20 kr</span> och inne på discot säljs diverse snacks och dryck i våra två kiosker
-        <br/>Du kan läsa mer om kioskerna och betalning under deras respektive rubriker
+        <br/>Inträdet är <span style={styles.bold}>20 kr</span> och inne på discot säljs diverse snacks och dryck i kiosken
+        <br/>Du kan läsa mer om kiosken och betalning under deras respektive rubriker
     </div>
   );
 }
--- client/src/tabs/mellanstadiedisco.js
+++ client/src/tabs/mellanstadiedisco.js
@@ -4,8 +4,8 @@ function mellanstadiedisco() {
   return (
     <div style={styles.text}>
         Discot för mellanstadieelever börjar <span style={styles.bold}>19:30</span> och slutar <span style={styles.bold}>21:15</span>
-        <br/>Inträdet är <span style={styles.bold}>20 kr</span> och inne på discot säljs diverse snacks och dryck i våra två kiosker
-        <br/>Du kan läsa mer om kioskerna och betalning under deras respektive rubriker
+        <br/>Inträdet är <span style={styles.bold}>20 kr</span> och inne på discot säljs diverse snacks och dryck i kiosken
+        <br/>Du kan läsa mer om kiosken och betalning under deras respektive rubriker
     </div>
   );
 }
