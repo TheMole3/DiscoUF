bcedcbb05d8ef5b7a923ea3f66343383aaf4f8aa
Hugo Arnlund <hugomelo04@gmail.com>
2022-09-09 10:13:42 +0200

    Changed about tab

--- client/src/tabs/omoss.js
+++ client/src/tabs/omoss.js
@@ -3,9 +3,8 @@ import styles from './tabStyle'
 function omoss() {
   return (
     <div style={styles.text}>
-        <img style={styles.image} src="../oss.jpg"></img>
-        <span style={styles.semiBold}>HÄR ÄR VI, TAGGADE PÅ ATT MÖTA ER!</span>
-        <br/>Du kanske känner igen några av oss från förra discot, annars hoppas vi att du får se oss när du kommer nästa gång.
+        <span style={styles.semiBold}>VI ÄR TAGGADE PÅ ATT MÖTA ER!</span>
+        <br/>Du kanske träffade oss på förra discot, annars hoppas vi att du får se oss när du kommer den här gången.
         <br/><br/>De som jobbar på discot är elever från Curt Nicolin Gymnasiet med Hugo Arnlund, Hugo Josefsson och Herman Svärd i spetsen.
         <br/>Ovannämda namn på H driver UF företaget Disco Melo UF som är arrangör för discot
     </div>
